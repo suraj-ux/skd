847331770f8eb3744b9603c61a98e10ea20455d2
Suraj Kumar <surajlink99@gmail.com>
2020-07-03 14:34:00 +0530

    video file add

---
+++ video.php
@@ -0,0 +1,51 @@
+<?php include('header.php'); ?>
+    <!-- ##### Breadcumb Area Start ##### -->
+    <div class="breadcumb-area bg-img bg-overlay2" style="background-image: url(img/bg-img/breadcumb2.jpg);">
+        <div class="bradcumbContent">
+            <h2>Tour Dates</h2>
+        </div>
+    </div>
+    <!-- bg gradients -->
+    <div class="bg-gradients"></div>
+    <!-- ##### Breadcumb Area End ##### -->
+
+    <!-- ##### Search Shows Area Start ##### -->
+    
+    <!-- ##### Search Shows Area End ##### -->
+
+    <!-- ##### Upcoming Shows Area Start ##### -->
+    
+    <!-- ##### Upcoming Shows Area End ##### -->
+
+    <!-- ##### Featured Shows Area Start ##### -->
+    
+    
+
+
+    <!-- ##### Featured Shows Area End ##### -->
+
+    <!-- ##### CTA Area Start ##### -->
+    <div class="musica-cta-area section-padding-100 bg-img bg-overlay2" style="background-image: url(img/blog-img/4.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="cta-content d-flex justify-content-between align-items-center">
+                        <div class="cta-text">
+                            <h4>Contact us now</h4>
+                            <h2>Do you have a question?</h2>
+                            <h6>Morbi quis venenatis augue, a tincidunt libero. Sed id porttitor elit, eu ultricies mauris.</h6>
+                        </div>
+                        <div class="cta-btn">
+                            <a href="#" class="btn musica-btn">elements</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### CTA Area End ##### -->
+
+    
+    <?php include('footer.php'); ?>
+
+</html>
